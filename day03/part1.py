753211700856e9e1f3221ff96de5df8b0dc7fc96
gabriela <sg.villegasq@gmail.com>
2024-12-03 22:48:53 +0000

    added day03-pt1 solution

---
+++ day03/part1.py
@@ -0,0 +1,22 @@
+from re import findall
+
+
+MUL_REGEX = "mul\(\d{1,3},\d{1,3}\)"
+
+
+def multiply_line(line: str) -> int:
+    matches = findall(MUL_REGEX, line)
+    total = 0
+    for m in matches:
+        numbers = list(map(int, m[4:-1].split(",")))
+        total += numbers[0] * numbers[1]
+    return total
+
+
+def solution(filename: str) -> int:
+    with open(filename, "r") as f:
+        return sum(multiply_line(line) for line in f)
+
+
+print(solution("input_test"))
+print(solution("input"))
